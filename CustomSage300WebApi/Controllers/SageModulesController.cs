bef588bf70ca938e4a3adadc5e67fd8ecfe9bf59
Ahercode <philipkelly407@gmail.com>
2024-04-04 19:14:52 +0000

    added new controller for SageModules

---
+++ CustomSage300WebApi/Controllers/SageModulesController.cs
@@ -0,0 +1,113 @@
+using AutoMapper;
+using CustomSage300WebApi.DBContext;
+using CustomSage300WebApi.Dtos;
+using CustomSage300WebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomSage300WebApi.Controllers;
+
+public class SageModulesController: ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly WorkFlowDbContext _context;
+    
+    public SageModulesController(IMapper mapper, WorkFlowDbContext context)
+    {
+        _mapper = mapper;
+        _context = context;
+    }
+    
+    [HttpGet]
+    public IActionResult GetAllSageModules()
+    {
+        var sageModules = _context.SageModules.ToList();
+        var sageModulesDto = _mapper.Map<IEnumerable<SageModuleResponse>>(sageModules);
+        return Ok(sageModulesDto);
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetASageModule(int id)
+    {
+        var sageModule = _context.SageModules.FirstOrDefault(x => x.Id == id);
+        var sageModuleDto = _mapper.Map<SageModuleResponse>(sageModule);
+        
+        if(sageModuleDto == null)
+            return NotFound("Sage Module not found");
+        
+        return Ok(sageModuleDto);
+    }
+    
+    [HttpPost]
+    public IActionResult CreateSageModule(SageModuleRequest createSageModuleRequest)
+    {
+        if(!ModelState.IsValid)
+            return BadRequest("Invalid data provided");
+
+        try
+        {
+            var sageModule = _mapper.Map<SageModule>(createSageModuleRequest);
+
+            if (sageModule.Code != null)
+            {
+                var sageModuleInDb = _context.SageModules.FirstOrDefault(x => x.Code == sageModule.Code);
+                if (sageModuleInDb != null)
+                    return BadRequest("Sage Module already exists");
+            }
+
+            _context.SageModules.Add(sageModule);
+            _context.SaveChanges();
+        
+            return Ok("Sage Module created successfully");
+            
+        }
+        catch (Exception e)
+        {
+            return BadRequest("An error occurred while creating Sage Module");
+        }
+    }
+    
+    [HttpPut("{id}")]
+    public IActionResult UpdateSageModule(int id, SageModuleRequest updateSageModuleRequest)
+    {
+        if(!ModelState.IsValid)
+            return BadRequest("Invalid data provided");
+
+        try
+        {
+            var sageModule = _context.SageModules.FirstOrDefault(x => x.Id == id);
+            if(sageModule == null)
+                return NotFound("Sage Module not found");
+
+            _mapper.Map(updateSageModuleRequest, sageModule);
+            _context.SaveChanges();
+        
+            return Ok("Sage Module updated successfully");
+            
+        }
+        catch (Exception e)
+        {
+            return BadRequest("An error occurred while updating Sage Module");
+        }
+    }
+    
+    [HttpDelete("{id}")]
+    public IActionResult DeleteSageModule(int id)
+    {
+        try
+        {
+            var sageModule = _context.SageModules.FirstOrDefault(x => x.Id == id);
+            if(sageModule == null)
+                return NotFound("Sage Module not found");
+
+            _context.SageModules.Remove(sageModule);
+            _context.SaveChanges();
+        
+            return Ok("Sage Module deleted successfully");
+            
+        }
+        catch (Exception e)
+        {
+            return BadRequest("An error occurred while deleting Sage Module");
+        }
+    }
+}
