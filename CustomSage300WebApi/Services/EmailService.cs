2c46bfc1696154b7b46d852f579d406b4bc0f664
Ahercode <philipkelly407@gmail.com>
2024-04-22 19:11:45 +0000

    updated the email service

--- CustomSage300WebApi/Services/EmailService.cs
+++ CustomSage300WebApi/Services/EmailService.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using CustomSage300WebApi.Interfaces;
 using MailKit.Net.Smtp;
 using MailKit.Security;
@@ -27,7 +28,29 @@ public class EmailService : IEmailService
         email.From.Add(MailboxAddress.Parse(from));
         email.To.Add(MailboxAddress.Parse(to));
         email.Subject = subject;
-        email.Body = new TextPart(TextFormat.Plain) { Text = body };
+        
+        // email.Body = new TextPart(TextFormat.Plain) { Text = body };
+       
+        var header = "<h2 style='color: #125B50; text-align: center;'>Engineers and Planners</h2>";
+        // var footer = "<img src='' alt='Footer Image'/>";
+
+        body = "<p>" + body.Replace(Environment.NewLine, "</p><p>") + "</p>";
+        
+        var builder = new BodyBuilder();
+        builder.HtmlBody = $@"
+        <html>
+            <body style='display: flex; justify-content: center; align-items: center;'>
+                <div style='background-color: #FFF7FC; border-radius: 15px; padding: 25px; width:600px; '>
+                    {header}
+                    {body}
+                    <p style='color: #FF0000; text-align: center; font-style: italic;'>This is a system generated email. Please do not reply to this email.</p>
+                    <p style='color: #4793AF; text-align: center; font-style: italic;'>Powered by <a href='https://www.linkedin.com/in/philipaherto/'>Ahercode</a></p>
+                </div>
+            </body>
+            
+        </html>";
+
+        email.Body = builder.ToMessageBody();
 
         try
         {
